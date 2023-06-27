2d9611251b9d7b532caac4477511c8c54ce32ab2
Mubin Faysal Uday <117937503+MubinFaysal@users.noreply.github.com>
2023-06-27 19:03:39 +0600

    Update phitron3.5.2.c

--- Problem_solve_with_C/phitron3.5.2.c
+++ Problem_solve_with_C/phitron3.5.2.c
@@ -1,1 +1,27 @@
+/*
+Write a C program that accepts two integers from the user and calculate the product of the two integers.
+Expected Input/Output:
+
+Input the first integer: 25
+
+Input the second integer: 15
+
+Expected Output:
+
+Product of the above two integers = 375
+*/
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Input the first integer: ");
+    scanf("%d",&a);
+    printf("Input the secondt integer: ");
+    scanf("%d",&b);
+
+    printf("Product of the above two integers = %d\n",a*b);
+
+
+    return 0;
+}
 
