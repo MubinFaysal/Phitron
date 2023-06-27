97b0a82dca7114b8bb48fe327a670a04946646d4
Mubin Faysal Uday <117937503+MubinFaysal@users.noreply.github.com>
2023-06-27 22:02:24 +0600

    Update phitron3.5.4.c

--- Problem_solve_with_C/phitron3.5.4.c
+++ Problem_solve_with_C/phitron3.5.4.c
@@ -1,1 +1,35 @@
+/*
+Write a C program to input principle, time and rate (P, T, R) from the user and find Simple Interest and principal plus interest.
+Example
+
+Input
+
+Enter principle: 1200
+
+Enter time: 2
+
+Enter rate: 5.4
+
+Output
+
+Simple Interest = 129.600006
+
+Principal + Interest = 1329.6
+*/
+#include<stdio.h>
+int main(){
+    float p,t,r,si;
+    printf("Enter principle: ");
+    scanf("%f",&p);
+    printf("Enter time:  ");
+    scanf("%f",&t);
+    printf("Enter rate:  ");
+    scanf("%f",&r);
+
+    si =(p*t*r)/100;
+
+    printf("Simple Interest = %f",si);
+
+    return 0;
+}
 
