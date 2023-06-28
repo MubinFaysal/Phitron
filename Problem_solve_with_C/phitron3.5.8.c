7289b10bbaf930d7ae2c83c9c2d49427cbd88204
Mubin Faysal Uday <117937503+MubinFaysal@users.noreply.github.com>
2023-06-28 15:24:15 +0600

    Update phitron3.5.8.c

--- Problem_solve_with_C/phitron3.5.8.c
+++ Problem_solve_with_C/phitron3.5.8.c
@@ -1,1 +1,30 @@
 
+/*
+Write a C program to input temperature in degree Fahrenheit and convert it to degree Centigrade.
+Example
+
+Input
+
+Temperature in fahrenheit = 205
+
+Output
+
+Temperature in celsius = 96.11 C
+
+
+
+*/
+
+#include<stdio.h>
+int main(){
+    float f,c;
+    printf("Temperature in fahrenheit = ");
+    scanf("%f",&f);
+
+    c =5*(f-32)/9;
+    printf("Temperature in celsius = %.2f C",c);
+
+
+    return 0;
+}
+
