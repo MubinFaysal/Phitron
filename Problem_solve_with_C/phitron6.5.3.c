d51599094080b497707fffb212c33fff4ec9d954
Mubin Faysal Uday <117937503+MubinFaysal@users.noreply.github.com>
2023-06-30 23:46:00 +0600

    Update phitron6.5.3.c

--- Problem_solve_with_C/phitron6.5.3.c
+++ Problem_solve_with_C/phitron6.5.3.c
@@ -1,1 +1,23 @@
+/*
+Write a C program to check whether a given non-negative number is a multiple of 3 or 7,
+ but not both.  For example: inputs 7, 3, 6, 14 should output yes.
+Inputting 21, 42 should output no.
+    */
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d",&a);
+
+    if(a%3==0 ^ a%7==0){
+        printf("yes\n");
+    }else{
+        printf("no\n");
+    }
+
+
+
+    return 0;
+}
+
 
